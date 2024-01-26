4cd9fd4fbeb5f2eb1b265a07068d09d0b04a1619
theCodelogy <thecodelogy.dev@gmail.com>
2024-01-26 17:22:14 +0600

    [search&filter] video and music search and filter functionality add in the video and music api

--- Controler/Video.js
+++ Controler/Video.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { MongoClient, ServerApiVersion,ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.DB_uri;
 const client = new MongoClient(uri, {
     serverApi: {
@@ -11,26 +11,62 @@ const client = new MongoClient(uri, {
 const videoCollection = client.db(process.env.DB_NAME).collection("Videos")
 
 
+// Get all Videos
 const getVideos = async (req, res) => {
-    const result = await videoCollection.find().toArray()
+    let query = {}
+    const limit = req.query.limit ? parseInt(req.query.limit) : 0;
+    const sort = {};
+    if (req.query.title) {
+        query.title = { $regex: new RegExp(req.query.title, 'i') }
+    }
+    if (req.query.category) {
+        query.category = { $regex: new RegExp(req.query.category, 'i') }
+    }
+    if (req.query.language) {
+        query.language = { $regex: new RegExp(req.query.language, 'i') }
+    }
+    if (req.query.hero) {
+        query.hero = { $regex: new RegExp(req.query.hero, 'i') }
+    }
+    if (req.query.tags) {
+        query.tags = { $regex: new RegExp(req.query.tags, 'i') }
+    }
+    if (req.query.sortby) {
+        sort[req.query.sortby] = req.query.sort ? parseInt(req.query.sort) : 1
+    }
+    const result = await videoCollection.find(query).sort(sort).limit(limit).toArray()
     res.send(result)
 }
 
+// Get single video by id
 const getSingleVideos = async (req, res) => {
-    console.log(req.params.id)
-    const query = {_id: new ObjectId(req.params.id)}
-    const result = await videoCollection.findOne(query)
-    res.send(result)
+    const query = { _id: new ObjectId(req.params.id) }
+    const exVideoData = await videoCollection.findOne(query)
+    const updateVideo = {
+        $set: {
+            view: exVideoData.view ? exVideoData.view + 1 : 1
+        }
+    }
+    const updateView = await videoCollection.updateOne(query, updateVideo)
+    const updateVideoData = await videoCollection.findOne(query)
+    res.send(updateVideoData)
 }
+
+
+// Upload Video
 const createVideo = async (req, res) => {
     const video = req.body;
     const result = await videoCollection.insertOne(video)
     res.send(result)
 }
-const deleteVideo = async(req,res)=>{
-    const query={_id: new ObjectId(req.params.id)}
+
+
+
+// Delete Video
+const deleteVideo = async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) }
     const result = await videoCollection.deleteOne(query)
     res.send(result)
 }
 
-module.exports = {getVideos,getSingleVideos,createVideo,deleteVideo}
+module.exports = { getVideos, getSingleVideos, createVideo, deleteVideo }
